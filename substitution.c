0820cb7778c71d12c78a4ad9502b0602e390cc73
Paul Trellis <paul.trellis@uon.edu.au>
2019-05-01 12:48:37 +0000

    some comments done...

--- substitution.c
+++ substitution.c
@@ -13,8 +13,8 @@ int findIndex(char code[],char charFind){
 }
 
 char* encrypt(char *message,char code[]){
-  int length = strlen(message);
-  char *encryptMessage = (char *) malloc(sizeof(char)*length);
+  int length = strlen(message); //takes the string "message" and counts the total elements in the array
+  char *encryptMessage = (char *) malloc(sizeof(char)*length); //changes the memory location 
 
   for(int i = 0; i < length; i++){
     int encryptIndex = toupper(message[i]) - 'A';
@@ -47,14 +47,14 @@ int x = 0;
 char message[1024];
 printf("Input message:\n");
 scanf("%[^\n]s", message);
-char code[26] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+char code[26] = "QWERTYUIOPASDFGHJKLZXCVBNM";   //encrypted alphabet that assigns A to Q, B to W, etc...
 
 printf("1. Encrypt the message\n2. Decrypt the message\n");
 scanf("%d", &x);
-switch(x)
+switch(x)   // simple menu for either option
     {
 case 1: {
-  char *encryptMessage = encrypt(message,code);
+  char *encryptMessage = encrypt(message,code); //uses the function "encrypt" to produce new message
   printf("Original Message: %s\nEncrypted Message: %s\n",message,encryptMessage);
   break;
 }
