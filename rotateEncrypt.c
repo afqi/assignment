a7e3fa52111454a5924bb46e5b51504419c6006f
Paul Trellis <paul.trellis@uon.edu.au>
2019-05-02 02:05:58 +0000

    finished commenting on the substitution cipher, ready for testing.

--- rotateEncrypt.c
+++ rotateEncrypt.c
@@ -1,16 +1,61 @@
-#include <stdio.h>
-
-int main ()
-// use '_' to represent a space
-{
-    int c = 0; //counter used for upcoming loop
-    int key = 2;
-    char str[50];
-    printf("Enter a message to be encrypted: ");
-    scanf("%s", str);
-        for (c>0; (c<40 && str[c] != '\0'); c++);
-        str[c] = str[c] + 2;
-    
-    printf("Encrypted message: %s\n", str);
-
-}
+//#include <stdio.h>
+//
+//int main ()
+//
+//{
+//    int n, x;
+//    int key = 1;
+//    char str[100];
+//    
+//    printf("Choose an option:\n1. Encryption\n2. Decryption\n");
+//    scanf ("%d", &x);
+//    
+//    switch(x)
+//    case 1:
+//{
+//    printf("Enter a message to be encrypted: ");
+//    scanf("%[^\n]s", str);
+//    
+//    for (n=0; (n<100 && str[n] != '\0'); n++)
+//    {
+//        if (str[n] == 'z' || str[n] == 'Z')
+//        {
+//        str[n] = 64;
+//        }
+//            if (str[n] <= 122 && str[n] >= 97)
+//            {
+//            str[n] = str[n] - 32;
+//            }
+//    str[n] = str[n] + key;
+//    }
+//    printf("Encrypted message: %s\n", str);
+//break;
+//
+//
+//      
+//    case 2:
+//        {
+//        printf("Enter a message to be decrypted: ");
+//        scanf("%[^\n]s", str);
+//    
+//        for (n=0; (n<100 && str[n] != '\0'); n++)
+//        {
+//            if (str[n] == 'z' || str[n] == 'Z')
+//            {
+//            str[n] = 64;
+//            }
+//                if (str[n] <= 122 && str[n] >= 97)
+//                {
+//                str[n] = str[n] - 32;
+//                }
+//        str[n] = str[n] - key;
+//        }
+//        printf("Decrypted message: %s\n", str);
+//break;
+//        }
+//default:
+//printf("No correct option was selected. Re-run the program.");
+//}
+//return 0;
+//
+//}
--- substitution.c
+++ substitution.c
@@ -6,20 +6,20 @@
 int findIndex(char code[],char charFind){
   for(int i = 0 ; i < 26; i ++){
     if(code[i] == charFind){
-      return i;
+      return i; //this function uses a for loop to find the index of an array that is used to assign the unencrypted letter to an encrypted one.
     }
   }
   return -1;
 }
 
-char* encrypt(char *message,char code[]){
+char* encrypt(char *message,char code[]){ //the encrypting function
   int length = strlen(message); //takes the string "message" and counts the total elements in the array
   char *encryptMessage = (char *) malloc(sizeof(char)*length); //changes the memory location 
 
   for(int i = 0; i < length; i++){
-    int encryptIndex = toupper(message[i]) - 'A';
-    if(encryptIndex >= 0 && encryptIndex < 26){
-      encryptMessage[i] = code[encryptIndex];
+    int encryptIndex = toupper(message[i]) - 'A'; //assigns message to encryptIndex and changes it to uppercase
+    if(encryptIndex >= 0 && encryptIndex < 26){ 
+      encryptMessage[i] = code[encryptIndex]; //takes the value of the array "i" and assigns it index to the matching index value in the encrypted code.
     }else{
       encryptMessage[i] = message[i];
     }
@@ -29,12 +29,12 @@ char* encrypt(char *message,char code[]){
 
 char *decrypt(char *message,char code[]){
   int length = strlen(message);
-  char *decryptMessage = (char *) malloc(sizeof(char)*length);
+  char *decryptMessage = (char *) malloc(sizeof(char)*length); //line 30,31,32 are the same as the encrypt function
   for(int i = 0; i <length; i++){
     int decryptIndex = toupper(message[i]) - 'A';
     if(decryptIndex >= 0 && decryptIndex < 26){
-      int codeIndex = findIndex(code,toupper(message[i]));
-      decryptMessage[i] = 'A' + codeIndex;
+      int codeIndex = findIndex(code,toupper(message[i])); //uses the findIndex function to find the index value in the encrypted alphabet and message, and makes that value an integer that will eventually revert the encryption.
+      decryptMessage[i] = 'A' + codeIndex; //adds the codeIndex to revert original encryption
     }else{
       decryptMessage[i] = message[i];
     }
@@ -55,16 +55,16 @@ switch(x)   // simple menu for either option
     {
 case 1: {
   char *encryptMessage = encrypt(message,code); //uses the function "encrypt" to produce new message
-  printf("Original Message: %s\nEncrypted Message: %s\n",message,encryptMessage);
+  printf("Original Message: %s\nEncrypted Message: %s\n",message,encryptMessage); //prints the returned message from the function
   break;
 }
 
 case 2: {
-  char *decryptMessage = decrypt(message,code);
-  printf("Decrypted Message: %s\n",decryptMessage);
+  char *decryptMessage = decrypt(message,code); //uses the function "decrypt"
+  printf("Decrypted Message: %s\n",decryptMessage); //prints the returned message from the function
   break;
 }
-default: printf("No option was selected. Re-run.");
+default: printf("No corrrect option was selected. Re-run."); //prints a message to tell user option 1 or 2 wasnt selected.
     }
         
     }
